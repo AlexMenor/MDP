3d930aaba98cbda27c7ce6a07072acd452b571ff
Alex Menor <alex4menor@gmail.com>
2020-02-28 21:37:59 +0100

    local search algorithm and basic menu

---
+++ local_search_algorithm/local_search_algorithm.go
@@ -0,0 +1,129 @@
+package local_search_algorithm
+
+import (
+	"math"
+	"math/rand"
+)
+
+func Compute (n, m int, distanceMatrix [][] float32) [] int {
+	selected, notSelected := generateRandomSets(n, m)
+	computeContribution(selected, distanceMatrix)
+
+	justImprovedSolution := true
+
+	for justImprovedSolution {
+		alreadyTried := make(map[int]bool)
+
+		for len(alreadyTried) < m {
+			minContributor, minContribution := findMinContributor(selected, alreadyTried)
+
+			candidate, candidateContribution, improvesSolution := tryCandidates(selected, notSelected, minContributor, minContribution, distanceMatrix)
+
+			if !improvesSolution {
+				alreadyTried[minContributor] = true
+				justImprovedSolution = false
+			} else {
+				delete(selected, minContributor)
+				selected[candidate] = candidateContribution
+				delete(notSelected, candidate)
+				notSelected[minContributor] = true
+				recalculateContributions(selected, candidate, minContributor, distanceMatrix)
+				justImprovedSolution = true
+				break
+			}
+		}
+	}
+
+	return setAsSlice(selected, m)
+}
+
+func recalculateContributions(selected map[int]float32, added int, removed int, distanceMatrix [][] float32) {
+	for selectedElem := range selected {
+		if selectedElem != added {
+			selected[selectedElem] -= distanceMatrix[selectedElem][removed]
+			selected[selectedElem] += distanceMatrix[selectedElem][added]
+		}
+	}
+}
+
+func tryCandidates (selected map[int]float32,  notSelected map[int]bool, minContributor int , minContribution float32, distanceMatrix [][]float32) (int, float32, bool) {
+	for candidate := range notSelected {
+		candidateContribution := computeCandidateContribution (candidate, minContributor, selected, distanceMatrix)
+
+		increment := candidateContribution - minContribution
+
+		if increment > 0 {
+			return candidate, candidateContribution, true
+		}
+	}
+
+	return 0, 0, false
+}
+
+func computeCandidateContribution (candidate, excluded int, selected map[int]float32, distanceMatrix [][]float32) float32{
+
+	var candidateContribution float32 = 0
+
+	for alreadySelected := range(selected) {
+		if alreadySelected != excluded {
+			candidateContribution += distanceMatrix[alreadySelected][candidate]
+		}
+	}
+
+	return candidateContribution
+
+}
+
+func findMinContributor (selected map[int]float32, alreadyTried map[int]bool) (int, float32) {
+	minContribution := float32(math.MaxFloat32)
+	var minSelected int
+
+	for selectedElem, contribution := range selected {
+		_, selectedWasTried	:= alreadyTried[selectedElem]
+		if !selectedWasTried && contribution < minContribution {
+			minContribution = contribution
+			minSelected = selectedElem
+		}
+	}
+
+	return minSelected, minContribution
+}
+
+func generateRandomSets(n int, m int) (map[int]float32, map[int]bool) {
+	selected := make(map[int]float32)
+	for len(selected) < m {
+		newSelected := rand.Intn(n)
+		selected[newSelected] = 0.0
+	}
+
+	notSelected := make(map[int]bool)
+
+	for i := 0 ; i < n ; i++ {
+		_, alreadySelected := selected[i]
+
+		if !alreadySelected {
+			notSelected[i] = true
+		}
+	}
+
+	return selected, notSelected
+}
+
+func computeContribution (selected map[int]float32, distanceMatrix [][] float32) {
+	for i := range selected {
+		var currentContribution float32 = 0
+		for j := range selected {
+			currentContribution += distanceMatrix[i][j]
+		}
+		selected[i] = currentContribution
+	}
+}
+
+func setAsSlice (selected map[int]float32, m int) []int {
+	sliceOfSelected := make([]int, 0, m)
+	for selectedElem := range selected {
+		sliceOfSelected = append(sliceOfSelected, selectedElem)
+	}
+
+	return sliceOfSelected
+}
--- main.go
+++ main.go
@@ -2,21 +2,78 @@ package main
 
 import (
 	"MDP/greedy_algorithm"
+	"MDP/local_search_algorithm"
 	"MDP/problem_reader"
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	file := "problem_instances/MDG-b_1_n500_m50.txt"
+	instancesNames := getArrayOfInstancesNames()
+
+	fmt.Println("Please choose a test!")
+	printInstancesNames (instancesNames)
+
+	choice := readChoice(len(instancesNames))
+
+	file := path.Join("problem_instances", instancesNames[choice])
 
 	n, m, distanceMatrix := problem_reader.ReadFile(file)
 
-	sol := greedy_algorithm.Compute(n,m, distanceMatrix)
+	printAlgorithmNames()
+
+	choice = readChoice(2)
+
+	var sol []int
+	switch choice {
+	case 0:
+		sol = greedy_algorithm.Compute(n,m, distanceMatrix)
+	case 1:
+		sol = local_search_algorithm.Compute(n,m, distanceMatrix)
+	}
 
 	fmt.Println(sol)
 	fmt.Println(getDiversity(sol, distanceMatrix, m))
 
+}
+
+func getArrayOfInstancesNames () []string {
+	f, err := os.Open("./problem_instances")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	instancesNames := make([]string, 0)
+
+	for _, file := range files {
+		instancesNames = append(instancesNames, file.Name())
+	}
+
+	return instancesNames
+}
+
+func printInstancesNames (instancesNames []string) {
+	for index, name := range instancesNames {
+		fmt.Printf("%d. %s\n", index, name)
+	}
+}
+
+func printAlgorithmNames () {
+	fmt.Println("0. Greedy Algorithm")
+	fmt.Println("1. Local Search Algorithm")
 }
 
 func getDiversity(selected []int, distanceMatrix [][]float32, m int) (diversity float32) {
@@ -28,4 +85,22 @@ func getDiversity(selected []int, distanceMatrix [][]float32, m int) (diversity 
 	}
 
 	return diversity
+}
+
+func readChoice (limit int) int {
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+
+	stringArr := strings.Split(text, "\n")
+
+	textChoice := stringArr[0]
+
+	choice, err := strconv.Atoi(textChoice)
+
+	if err != nil || choice < 0 || choice > limit {
+		fmt.Print("That's not a valid choice!")
+		os.Exit(1)
+	}
+
+	return choice
 }
